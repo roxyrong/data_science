73f20d112894eb3c6ce1ad95cd2c14efb074ba6e
roxyrong <rongxinyun2010@gmail.com>
2018-07-22 15:24:59 -0500

    add polynomial func

--- Assessing Performance/assessing-fit.py
+++ Assessing Performance/assessing-fit.py
@@ -17,3 +17,14 @@ predicted = regr1.predict(np.array(poly_data1['sqft_living']).reshape(-1, 1))
 plt.plot(poly_data1['sqft_living'], poly_data1['price'], '.',
          poly_data1['sqft_living'], predicted, '-')
 
+# polynomial regression
+poly_data2 = sales[['sqft_living', 'price']].sort_values(['sqft_living', 'price'])
+X = np.array(poly_data2['sqft_living']).reshape(-1, 1)
+poly = PolynomialFeatures(2)
+X = poly.fit_transform(X)
+y = np.array(poly_data2['price'])
+regr2 = LinearRegression()
+regr2.fit(X, y)
+predicted = regr2.predict(X)
+plt.plot(poly_data2['sqft_living'], poly_data2['price'], '.',
+         poly_data2['sqft_living'], predicted, '-')
