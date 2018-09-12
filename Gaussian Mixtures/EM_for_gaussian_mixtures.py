ac387fa55a2ab640d10b3a903c9beebd8a91dd71
roxyrong <rongxinyun2010@gmail.com>
2018-09-11 23:09:24 -0500

    image clusters

--- Gaussian Mixtures/EM_for_gaussian_mixtures.py
+++ Gaussian Mixtures/EM_for_gaussian_mixtures.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from scipy.stats import multivariate_normal
-import copy
-
+import colorsys
 from PIL import Image
-from io import BytesIO
 import glob
 
 
@@ -15,7 +14,7 @@ def pull_image_data():
         im = np.array(Image.open(filename).getdata())
         rgb = [np.mean(im[:, 0] / 256.0), np.mean(im[:, 1] / 256.0), np.mean(im[:, 2] / 256.0)]
         image_list.append(rgb)
-    return image_list
+    return np.array(image_list)
 
 
 def log_sum_exp(Z):
@@ -136,4 +135,48 @@ def em(data, init_means, init_covariances, init_weights, maxiter=1000, thresh=1e
         print("Iteration %s" % it)
     out = {'weights': weights, 'means': means, 'covs': covariances, 'loglik': ll_trace, 'resp': resp}
     return out
+
+
+def plot_loglikelihood(out):
+    ll = out['loglik']
+    plt.plot(range(len(ll)), ll, linewidth=4)
+    plt.xlabel('Iteration')
+    plt.ylabel('Log-likelihood')
+    plt.rcParams.update({'font.size': 16})
+    plt.tight_layout()
+    return
+
+
+def plot_responsibilities_in_RB(r, g ,b, resp, title):
+    N, K = resp.shape
+
+    hsv_tuples = [(x * 1.0 / K, 0.5, 0.9) for x in range(K)]
+    rgb_tuples = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples))
+
+    resp_by_img_int = [[resp[n][k] for k in range(K)] for n in range(N)]
+    cols = [tuple(np.dot(resp_by_img_int[n], np.array(rgb_tuples))) for n in range(N)]
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    for n in range(len(r)):
+        ax.scatter(r[n], g[n], b[n], c=cols[n], marker='o')
+    ax.set_xlabel('R value')
+    ax.set_ylabel('G value')
+    ax.set_zlabel('B value')
+    return
+
+
+def images_clusters(iteration=100):
+    image_data = pull_image_data()
+    r, g, b = image_data[:, 0], image_data[:, 1], image_data[:, 2]
+    np.random.seed(1)
+    init_means = np.array([image_data[x] for x in np.random.choice(len(image_data), 4, replace=False)])
+    cov = np.diag([r.var(), g.var(), b.var()])
+    init_covariances = [cov, cov, cov, cov]
+    init_weights = [1/4., 1/4., 1/4., 1/4.]
+    out = em(image_data, init_means, init_covariances, init_weights, maxiter=iteration)
+    plot_loglikelihood(out)
+    plot_responsibilities_in_RB(r, g, b, out['resp'], 'After ' + str(len(out['loglik'])) + ' iteration')
+    return
+
 
